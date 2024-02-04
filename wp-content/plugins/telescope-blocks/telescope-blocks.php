adec5ab406a27db26a3741411dc1ab4b5410e04e
iroshiva <jean.weil@protonmail.com>
2024-02-04 14:39:07 +0100

    STYLE - Common.scss didn't work

--- wp-content/plugins/telescope-blocks/telescope-blocks.php
+++ wp-content/plugins/telescope-blocks/telescope-blocks.php
@@ -4,9 +4,11 @@
  * Plugin Name:       Telescope Blocks
  * Description:       Plugin pour les blocks gutenberg Hero Section / Half Section / Posts Section / Newsletter Section
  * Requires at least: 6.1
- * Requires PHP:      8.0
+ * Requires PHP:      7.0
  * Version:           0.1.0
  * Author:            Weil Jean
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       telescope-blocks
  *
  * @package           create-block
